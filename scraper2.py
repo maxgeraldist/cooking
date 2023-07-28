00a7c0c52e2a3d74b4779863f3eec3cd3962decb
Максим Ефимов <maxgeraldist@gmail.com>
2023-07-28 18:21:25 +0200

    Finally got scraper2 to work, although speed remains problematic
    Former-commit-id: 6bcedcf03f0081a6d7affde7988bc91c5a52e5f6

--- scraper2.py
+++ scraper2.py
@@ -1,71 +1,87 @@
-import pandas as pd
 import json
 import re
 import time
 import os
-
+import pandas as pd
+start_time = time.time()
 def get_numerator_denominator(string):
     match = re.search(r'(\d+)/(\d+)', string)
     if match:
-        return match.group(1), match.group(2)
+        numerator = int(match.group(1))
+        denominator = int(match.group(2))
+        return numerator, denominator
     return None, None
-
-start_time = time.time()
+measurement_units = ["cup", "cups", "teaspoon", "teaspoons", "tablespoon", "tablespoons", "ounce","-ounce","-ounces", "ounces", "pound", "pounds", "clove", "cloves", "inch", "inches","can","cans","package","packages", "pinch","pinches"]
 def extract_ingredient_data(row, measurement_units):
-    amount = ""
+    amount = 0
     measurement = ""
     ingredient_name = ""
-    if '/' in row[0]:
-        numerator, denominator = get_numerator_denominator(row[0])
-        fraction = float(numerator) / float(denominator) if numerator is not None and denominator is not None else 0
-        row[0] = re.sub(r'(\d+)/(\d+)', '', row[0])
-    words = row[0].split(" ")
+    fraction = 0
+    if row is None:
+        return pd.Series([amount, measurement, ingredient_name])
+    # print('test0',row)
+    if '/' in row:
+        numerator, denominator = get_numerator_denominator(row)
+        fraction = float(float(numerator) / float(denominator)) if denominator is not None else 0
+        row = re.sub((str(numerator) + '/' + str(denominator)), '', row)
+    # print('test1',row)
+    words = row.split(" ")
+    # print('test2',words)
     for word in words:
         word=word.replace("-"," ")
         if word in measurement_units:
             measurement = word
+            words.remove(word)
+    # print('test3',words)
     if words[0].isdigit():
-        amount = words[0]
-        ingredient_name = " ".join(words[1:])
-    elif words[0].isdigit() == False and measurement == "":
-        amount = "1"
-        ingredient_name = " ".join(words)
- 
-    return pd.Series([amount, measurement, ingredient_name])# Define the measurement units we want to extract
-measurement_units = ["cup", "cups", "teaspoon", "teaspoons", "tablespoon", "tablespoons", "ounce","-ounce","-ounces", "ounces", "pound", "pounds", "clove", "cloves", "inch", "inches"]
+        amount = int(words[0])
+        words.remove(words[0])
+    elif fraction != 0:
+        amount = float(fraction)
+    else:
+        amount = 1
+    # print('test4',words)
+    ingredient_name = " ".join(words)
+    return pd.Series([amount, measurement, ingredient_name])
+# Define the measurement units we want to extract
 def process_file(filename, rows): 
     with open(filename, "r") as f:
         # Read the contents of the input file into a string
         file_contents = f.read()
-        # Remove or escape any invalid control characters
         file_contents = re.sub(r'[\x00-\x1f\x7f-\x9f]', '', file_contents)
         # Parse the modified string using json.loads
         data = json.loads(file_contents) 
         # Iterate over the data and extract the information
     for recipe_id, recipe_data in data.items():
-        instructions = recipe_data.get("instructions", "")
+        instructions = recipe_data.get("instructions", "")     
+        # this is so stupid but it doesn't work otherwise - ascii remover doesn't work and decode-encode is way to slow
+        # instructions = ''.join([char for char in instructions if ord(char) < 128])
         recipe_name = recipe_data.get("title", "")
         ingredients_list = recipe_data.get("ingredients", [])
+        # print(ingredients_list, 'ingredients_list')
         if not ingredients_list :
             continue
         ingredients = pd.Series(ingredients_list)
         ingredients = ingredients.str.replace('\"ADVERTISEMENT\"', '', regex=True)
         ingredients = ingredients.str.replace(r'\([^)]*\)|ADVERTISEMENT', '', regex=True)
-        parts = ingredients.str.split(expand=True)
+        #remove empty rows
+        ingredients = ingredients[ingredients != '']
+        # ingredients = ingredients.apply(lambda x: ''.join([char for char in x if ord(char) < 128]))
         # Extract the amount, measurement, and ingredient name from the parts 
-        data = parts.apply(lambda row: extract_ingredient_data(row, measurement_units), axis=1)
+        if any(elem is not None for elem in recipe_data):
+            data = ingredients.apply(lambda x: extract_ingredient_data(x, measurement_units))
         data.columns = ["amount", "measurement", "ingredient_name"]
         amount = data["amount"]
         measurement = data["measurement"]
         ingredient_name = data["ingredient_name"]
         # Create a DataFrame from the extracted data
         data = {
-            "recipe_id": [instructions if i == 0 else "" for i in range(len(ingredients))],
+            "recipe_id": [recipe_id if i == 0 else "" for i in range(len(ingredients))],
+            "recipe_name": [recipe_name if i == 0 else "" for i in range(len(ingredients))],
             "ingredient": ingredient_name,
             "amount": amount,
             "measurement": measurement,
             "instructions": [instructions if i == 0 else "" for i in range(len(ingredients))],
-            "recipe_name": [recipe_name if i == 0 else "" for i in range(len(ingredients))]
         }
         new_df = pd.DataFrame(data)
         # Append all rows to the rows list
@@ -73,16 +89,18 @@ def process_file(filename, rows):
 rows = []
 i = 0
 # Loop through each input file and process it
-
+csv_or_xlsx = input("Press 1 for csv or 2 for xlsx outputs: ")
+# remap the input to the correct file extension
+if csv_or_xlsx == '1':
+    cs_or_xlsx = 'csv'
+else:
+    cs_or_xlsx = 'xlsx'
 input_dir = 'Input_files'
 input_files = [os.path.join(input_dir, name) for name in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, name))]
 # Process each input file separately (the dataframe is too big for the excel file format))
 for i, input_file in enumerate(input_files, start=1): 
     process_file(input_file, rows)
     df = pd.DataFrame(rows)
+    df.to_csv(("output" + str(i) + cs_or_xlsx), index=False)
     print(str(i)+" files processed")
-    df.to_excel("recipes"+str(i)+".xlsx", index=False)
-    rows = []
-    end_time = time.time()
-print("Time taken to run this cell :", end_time - start_time, "seconds.")
-print("Number of errors: ", error_count)
+print("Total time: %s seconds" % (time.time() - start_time))

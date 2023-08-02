5d005f508605025a21da9574cf772ba9b28d94af
Максим Ефимов <maxgeraldist@gmail.com>
2023-08-02 23:30:05 +0200

    Cleaner is still work in progress

--- cleaner.py
+++ cleaner.py
@@ -1,48 +1,40 @@
 import pandas as pd
 import os
-import numpy as np
 import time
 
 start_time = time.time()
-m=0
-def clean_instructions(row, df, instructions):
-   # Move the instructions to a separate table
+
+def clean_instructions(df, instructions):
     for i in range(len(instructions)):
-        if instructions['Instruction'][i] in row['Ingredient']:
-            row['Instruction'] = instructions['ID'][i]
-            row['Ingredient'] = row['Ingredient'].replace(instructions['Instruction'][i], '')
-            break
-
-    return row
-def clean_ingredients(row, ingredients):
-    # replace all the ingredients with their ID and remove the rest of the string
-    for i in range(len(ingredients)):
-        if ingredients['Ingredient'][i] in row['Ingredient']:
-            row['Ingredient'] = ingredients['ID'][i]
-            m+=1
-            if m%1000==0:
-                print('ingredients processed: ', m)
-            break
-    return row
-instructions = pd.DataFrame({'Instruction': ['minced', 'chopped', 'diced', 'sliced', 'grated', 'peeled', 'crushed', 'melted', 'mashed']})
-instructions['ID'] = instructions.index
-
-def clean_data(df):
-    # apply the cleaning functions to the dataframe
-    df = df.apply(clean_instructions, axis=1, args=(df, instructions))
-    ingredients = pd.DataFrame({'Ingredient': df['Ingredient'].unique()})
-    ingredients['ID'] = ingredients.index
-    df = df.apply(clean_ingredients, axis=1, args=(ingredients,))
-    df = df.dropna()
-    df = df[df['Ingredient'] != '']
+        mask = df['ingredient'].str.contains(instructions['Instruction'][i])
+        df.loc[mask, 'Instruction'] = instructions['ID'][i]
+        df.loc[mask, 'ingredient'] = df.loc[mask, 'ingredient'].str.replace(instructions['Instruction'][i], '')
     return df
 
+def clean_ingredients(df, ingredients):
+    # replace all the ingredients with their id and remove the rest of the string
+    for i in range(len(ingredients)):
+        mask1 = df['ingredient'].str.contains(ingredients['ingredient'][i], na=False)
+        df.loc[mask1, 'ingredient'] = ingredients['ID'][i]
+    return df
 
+instructions = pd.DataFrame({'Instruction': ['minced', 'chopped', 'diced', 'sliced', 'grated', 'peeled', 'crushed', 'melted', 'mashed']})
+instructions['ID'] = instructions.index
+ingredients = pd.DataFrame(columns=['ID', 'ingredient'])
+def clean_data(df, ingredients):
+    # apply the cleaning functions to the dataframe
+    df = clean_instructions(df, instructions)
+    ingredients = pd.concat([ingredients['ingredient'], pd.DataFrame({'ingredient': df['ingredient'].unique()})])
+    df = clean_ingredients(df, ingredients)
+    df = df.dropna(subset=['ingredient'])
+    df = df[df['ingredient'] != '']
+    return df
 
 input_dir = 'Recipes'
 output_dir = 'Cleaned_Recipes'
 input_files = [os.path.join(input_dir, name) for name in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, name))]
 # Read all files and concatenate them into one dataframe
+i = 0
 for input_file in input_files:
     rows=[]
     if '.xlsx' in input_file:
@@ -53,18 +45,27 @@ for input_file in input_files:
         format = False
     else:
         continue
-    df = df[df['Ingredient'].notnull()]
-    df['Ingredient'] = df['Ingredient'].strip(' ')
-    df['Ingredient'] = df['Ingredient'].str.lower()
-    df['Ingredient'] = df['Ingredient'].str.replace(',', '')
-    df['Ingredient'] = df['Ingredient'].str.replace('.', '')
-    df['Ingredient'] = df['Ingredient'].str.replace('(', '')
-    df['Ingredient'] = df['Ingredient'].str.replace(')', '')
-    clean_data(df)
+    df = df[df['ingredient'].notnull()]
+    df['ingredient'] = df['ingredient'].str.strip(' ')
+    df['ingredient'] = df['ingredient'].str.lower()
+    df['ingredient'] = df['ingredient'].str.replace(',', '')
+    df['ingredient'] = df['ingredient'].str.replace('.', '')
+    df['ingredient'] = df['ingredient'].str.replace('(', '')
+    df['ingredient'] = df['ingredient'].str.replace(')', '')
+    clean_data(df, ingredients)
     output_file = os.path.join(output_dir, os.path.basename(input_file))
     if format == 'xlsx':
-        df.to_excel(output_file, index=False)
+        df.to_excel('input_file'+str(i), index=False)
     else:
-        df.to_csv(output_file, index=False)
+        df.to_csv('output_file' +str(i), index=False)
+
+ingredients['ID'] = ingredients.index
+if format == 'csv':
+    ingredients.to_csv('ingredients.csv', index=False)
+    instructions.to_csv('instructions.csv', index=False)
+else:
+    ingredients.to_excel('ingredients.xlsx', index=False)
+    instructions.to_excel('instructions.xlsx', index=False)
 end_time = time.time()
 print('Time taken to clean data: ', end_time - start_time)
+
